4638f2c1a0e85dcdfbfe41020930112c25e57132
Thesoap <tvh_1994@hotmail.dk>
2016-11-29 14:10:21 +0100

    API facade kald start

--- ExamPoFE/src/main/java/Facades/FlightInstanceFacade.java
+++ ExamPoFE/src/main/java/Facades/FlightInstanceFacade.java
@@ -7,6 +7,8 @@ package Facades;
 
 import Entity.Flight;
 import Entity.FlightInstance;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -54,5 +56,15 @@ public class FlightInstanceFacade {
         em.getTransaction().commit();
         em.close();
         return true;
+    }
+
+    public List<FlightInstance> getFromAPI(String from, String date, int tickets) {
+        EntityManager em = emf.createEntityManager();
+        List<FlightInstance> FI = new ArrayList();
+        
+        em.getTransaction().begin();
+        em.getTransaction().commit();
+        return FI;
+        
     }
 }
