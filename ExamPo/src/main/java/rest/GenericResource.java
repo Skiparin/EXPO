ee694cfae0c88e58c571a5d27f8eea99f0bf5db0
Skiparin <Og1806x9@hotmail.com>
2016-11-30 14:14:20 +0100

    refactored api so we use streams and futures

--- ExamPo/src/main/java/rest/GenericResource.java
+++ ExamPo/src/main/java/rest/GenericResource.java
@@ -8,14 +8,8 @@ package rest;
 import com.google.gson.Gson;
 import entity.Airline;
 import facades.AirlineFacade;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
-import static java.util.Collections.list;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.ws.rs.core.Context;
@@ -25,15 +19,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import runnable.HttpGetTask;
-import java.util.concurrent.ExecutorService; 
-import java.util.concurrent.Executors; 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import javax.ws.rs.Consumes;
 
 /**
  * REST Web Service
@@ -66,131 +57,85 @@ public class GenericResource {
      */
     @GET
     @Produces(MediaType.TEXT_HTML)
-    @Path("/todo/{from}/{to}/{date}/{persons}")
-    public String getFlights(@PathParam("from") String FROM, @PathParam("to") String TO,
-            @PathParam("date") String DATE, @PathParam("persons") String PERSONS) {
-        List<Airline> list = facade.getAllAirlines();
-        String json = "[";
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getUrl() != null) {
-
-                try {
-                    DefaultHttpClient httpClient = new DefaultHttpClient();
-
-                    HttpGet GetRequest = new HttpGet(list.get(i).getUrl() + FROM + "/" + TO + "/" + DATE + "/" + PERSONS);
-                    System.out.println(GetRequest.getURI());
-                    HttpResponse response = httpClient.execute(GetRequest);
-
-                    BufferedReader br = new BufferedReader(
-                            new InputStreamReader((response.getEntity().getContent())));
-
-                    String output;
-                    while ((output = br.readLine()) != null) {
-                        json = json.concat(output);
-                        System.out.println(json);
-                    }
-                    if ((i + 1) != list.size()) {
-                        json = json.concat(",");
-                    }
-                    httpClient.getConnectionManager().shutdown();
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        json = json.concat("]");
-        return json;
-    }
-
-    @GET
-    @Produces(MediaType.TEXT_HTML)
-    @Path("/todo/{from}/{date}/{persons}")
+    @Path("/get/{from}/{date}/{persons}")
     public String getFlights(@PathParam("from") String FROM, @PathParam("date") String DATE,
             @PathParam("persons") String PERSONS) {
-        String json = "";
-        try {
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpGet GetRequest = new HttpGet("http://airline-plaul.rhcloud.com/api/flightinfo/" + FROM + "/" + DATE + "/" + PERSONS);
-
-            HttpResponse response = httpClient.execute(GetRequest);
-
-            BufferedReader br = new BufferedReader(
-                    new InputStreamReader((response.getEntity().getContent())));
-
-            String output;
-            while ((output = br.readLine()) != null) {
-                json = json.concat(output);
-                System.out.println(json);
-            }
-            httpClient.getConnectionManager().shutdown();
-
-        } catch (MalformedURLException e) {
-
-            e.printStackTrace();
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-
-        }
-        return json;
-    }
-
-    @GET
-    @Produces(MediaType.TEXT_HTML)
-    @Path("/todoo/{from}/{to}/{date}/{persons}")
-    public String getFlightss(@PathParam("from") String FROM, @PathParam("to") String TO,
-            @PathParam("date") String DATE, @PathParam("persons") String PERSONS) {
         List<Airline> list = facade.getAllAirlines();
-        List<Future> futures = new ArrayList<>();
+        List<Future<String>> futures = new ArrayList<>();
         ExecutorService threadpool = Executors.newFixedThreadPool(list.size());
-        String params = FROM + "/" + TO + "/" + DATE + "/" + PERSONS;
+        String params = FROM + "/" + DATE + "/" + PERSONS;
         String json = "[";
-        
+
         for (Airline airline : list) {
             futures.add(threadpool.submit(new HttpGetTask(airline.getUrl() + params)));
         }
-        
-        while(!allDone(futures)){
+
+        String smarterJson = futures.stream().<String>map(future -> {
             try {
-                Thread.sleep(100);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(GenericResource.class.getName()).log(Level.SEVERE, null, ex);
+                return future.get();
+            } catch (Exception ex) {
+                throw new RuntimeException("");
             }
-        }
- 
-        for (Future future : futures) {
-            try {
-                json = json.concat((String) future.get());
-                json = json.concat(",");
-            } catch (InterruptedException ex) {
-                Logger.getLogger(GenericResource.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (ExecutionException ex) {
-                Logger.getLogger(GenericResource.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        json = json.substring(0, json.length() - 1);
-        json = json.concat("]");
+        }).parallel().collect(Collectors.joining(","));
+
+        json = json.concat(smarterJson + "]");
         return json;
-        
     }
 
-    
-    public boolean allDone(List<Future> list){
-        int size = list.size();
-        int count = 0;
-        
-        for (Future task : list) {
-            if(task.isDone()){
-                count++;
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("/get/{from}/{to}/{date}/{persons}")
+    public String getFlights(@PathParam("from") String FROM, @PathParam("to") String TO,
+            @PathParam("date") String DATE, @PathParam("persons") String PERSONS) {
+        List<Airline> list = facade.getAllAirlines();
+        List<Future<String>> futures = new ArrayList<>();
+        ExecutorService threadpool = Executors.newFixedThreadPool(list.size());
+        String params = FROM + "/" + TO + "/" + DATE + "/" + PERSONS;
+        String json = "[";
+
+        for (Airline airline : list) {
+            futures.add(threadpool.submit(new HttpGetTask(airline.getUrl() + params)));
+        }
+
+        String smarterJson = futures.stream().<String>map(future -> {
+            try {
+                return future.get();
+            } catch (Exception ex) {
+                throw new RuntimeException("");
             }
-        }
-        if(size == count){
-            return true;
-        }
-        return false;
+        }).parallel().collect(Collectors.joining(","));
+
+        json = json.concat(smarterJson + "]");
+        return json;
     }
+    
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/get/{from}/{to}/{date}/{persons}")
+    public String getFlights(@PathParam("from") String FROM, @PathParam("to") String TO,
+            @PathParam("date") String DATE, @PathParam("persons") String PERSONS) {
+        List<Airline> list = facade.getAllAirlines();
+        List<Future<String>> futures = new ArrayList<>();
+        ExecutorService threadpool = Executors.newFixedThreadPool(list.size());
+        String params = FROM + "/" + TO + "/" + DATE + "/" + PERSONS;
+        String json = "[";
+
+        for (Airline airline : list) {
+            futures.add(threadpool.submit(new HttpGetTask(airline.getUrl() + params)));
+        }
+
+        String smarterJson = futures.stream().<String>map(future -> {
+            try {
+                return future.get();
+            } catch (Exception ex) {
+                throw new RuntimeException("");
+            }
+        }).parallel().collect(Collectors.joining(","));
+
+        json = json.concat(smarterJson + "]");
+        return json;
+
+    }
+
 }
